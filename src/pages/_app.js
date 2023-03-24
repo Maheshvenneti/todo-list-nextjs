948e29a70a122687c6c753f19738cc9c8ee17746
mahesh <maheshvenneti123@gmail.com>
2023-03-24 21:07:28 +0530

    commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,24 @@
-import '@/styles/globals.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/globals.css";
+import { useEffect, createContext, useState } from "react";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export const store = createContext();
+
+function MyApp({ Component, pageProps }) {
+
+  const [ID, setID] = useState();
+
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
+  return (
+    <store.Provider value={[ID, setID]}>
+    <Component {...pageProps} />;
+    </store.Provider>
+  )
+
+  
 }
+
+export default MyApp;
